185c2f3721d79c0a46d0f7bdc528ec7a4f4bd36e
Sébastien D <sebastien.delahaye.contact@gmail.com>
2024-09-20 18:38:39 +0200

    Interface and library
    Creation of the interfaces and start the installation of the library for charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { NgApexchartsModule } from "ng-apexcharts";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, NgApexchartsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { ChartComponent, ApexResponsive, ApexChart } from 'ng-apexcharts';
+
+export type ChartOptions = {
+    chart: ApexChart;
+    responsive: ApexResponsive[];
+    labels: any;
+};
 
 @Component({
   selector: 'app-home',
@@ -8,11 +15,22 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+    public olympics$: Observable<any> = of(null);
+    public chartOptions: Partial<any>;
+    public chart: ApexChart = { type: 'bar' };
 
-  constructor(private olympicService: OlympicService) {}
+    constructor(private olympicService: OlympicService) {
+        this.chartOptions = {
+            series: [96, 54, 345, 125, 113],
+            labels: ["Italy", "Spain", "United States", "Germany", "France"],
+            chart: {
+                width: 400,
+                type: "pie",
+            }
+        };
+    }
 
-  ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
-  }
+    ngOnInit(): void {
+        this.olympics$ = this.olympicService.getOlympics();
+    }
 }
