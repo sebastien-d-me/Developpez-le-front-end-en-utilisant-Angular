ce284427386e681e93697e0f2e0c1612eafbc581
Sébastien D <sebastien.delahaye.contact@gmail.com>
2024-09-27 21:43:37 +0200

    💄 UI update
    - Add the UI for the dashboard page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { OlympicService } from "src/app/core/services/olympic.service";
 import { ApexNonAxisChartSeries, ApexChart, ApexLegend } from "ng-apexcharts";
-import { HttpClient } from "@angular/common/http";
 
 export type ChartOptions = {
     series: ApexNonAxisChartSeries;
@@ -21,22 +20,38 @@ export class HomeComponent implements OnInit {
     public chartOptions: Partial<any>;
     public chart: ApexChart = { type: "pie" };
 
-    constructor(private http: HttpClient, private olympicService: OlympicService) {
-        this.http.get("/assets/mock/olympic.json").subscribe(config => {
-            console.log(config);
-        });
-
+    constructor(private olympicService: OlympicService) {
         this.chartOptions = {
             series: [96, 54, 345, 125, 113],
             labels: ["Italy", "Spain", "United States", "Germany", "France"],
             chart: {
-                height: 500,
+                height: 1000,
                 type: "pie",
-                width: 500,
+                width: 1000,
             },
             legend: {
                 show: false,
             },
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    options: {
+                        chart: {
+                            height: 500,
+                            width: 500,
+                        },
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    options: {
+                        chart: {
+                            height: 300,
+                            width: 300,
+                        },
+                    }
+                },
+            ]
         };
     }
 
