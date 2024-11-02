2a1e8e231cbff1b5e1488a4beab22b05d106862c
Sébastien D <sebastien.delahaye.contact@gmail.com>
2024-11-02 18:05:40 +0100

    ♻️ Cleaning code
    - Progress the cleaning code

--- src/app/pages/details/details.component.spec.ts
+++ src/app/pages/details/details.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DetailsComponent } from "./details.component";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { DetailsComponent } from './details.component';
 
-describe('DetailsComponent', () => {
-  let component: DetailsComponent;
-  let fixture: ComponentFixture<DetailsComponent>;
+describe("DetailsComponent", () => {
+    let component: DetailsComponent;
+    let fixture: ComponentFixture<DetailsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DetailsComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [DetailsComponent]
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(DetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(DetailsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,73 +1,55 @@
 import { Component, OnInit } from "@angular/core";
+import { ApexNonAxisChartSeries, ApexChart, ApexDataLabels, ApexLegend, ApexTooltip } from "ng-apexcharts";
 import { Observable, of } from "rxjs";
 import { OlympicService } from "src/app/core/services/olympic.service";
-import { ApexNonAxisChartSeries, ApexChart, ApexLegend } from "ng-apexcharts";
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router } from "@angular/router";
+
 
 export type ChartOptions = {
     series: ApexNonAxisChartSeries;
     chart: ApexChart;
-    labels: any;
+    labels: ApexDataLabels;
     legend: ApexLegend;
-    tooltip: any;
+    tooltip: ApexTooltip;
 };
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"],
+    selector: "app-home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+    public numberJo: number;
+
+
     public olympics$: Observable<any> = of(null);
-    public olympicCountries: any;
-    public olympicsJo: number;
-    public countriesMedal: any;
-    public countriesMedals: any;
+    
+
+
+
+
+    
 
     public chartOptions: Partial<any>;
     public chart: ApexChart = { type: "pie" };
 
-    constructor(private olympicService: OlympicService, private router: Router, private route: ActivatedRoute) {
-        this.olympicCountries = [];
-        this.countriesMedal = [];
-        this.countriesMedals = [];
+    constructor(private olympicService: OlympicService, private router: Router) {
+        this.numberJo = 0;
 
         this.chartOptions = {
             chart: {
-                height: 600,
-                type: "pie",
-                width: 600,
                 events: {
                     dataPointSelection: (event: any, chartContext: any, opts: any) => {
-                        /*console.log(opts.w.config.labels[opts.dataPointIndex]);
-                        console.log(opts.w.config.series[opts.dataPointIndex]);*/
                         this.router.navigate(["/details", opts.dataPointIndex + 1]);
                     }
-                }
+                },
+                height: 600,
+                type: "pie",
+                width: 600
             },
+            labels: [],
             legend: {
                 position: "bottom"
-            },
-            tooltip: {
-                custom: function(opts: any) {
-                    return `
-                    <style>
-                        .chart__tooltip {
-                            background-color: #04838F;    
-                            display: flex;
-                            flex-direction: column;
-                            gap: 2px 0;
-                            min-width: 60px;
-                            padding: 5px 10px;
-                            text-align: center;
-                        }
-                    </style>
-
-                    <div class="chart__tooltip">
-                        <span>${opts.w.globals.labels[opts.seriesIndex]}</span>
-                        <span>🎖️ ${opts.series[opts.seriesIndex]}</span>
-                    </div>`;
-                }
             },
             responsive: [
                 {
@@ -75,7 +57,7 @@ export class HomeComponent implements OnInit {
                     options: {
                         chart: {
                             height: 500,
-                            width: 500,
+                            width: 500
                         },
                     }
                 },
@@ -84,52 +66,57 @@ export class HomeComponent implements OnInit {
                     options: {
                         chart: {
                             height: 300,
-                            width: 300,
+                            width: 300
                         },
                     }
                 },
-            ]
+            ],
+            series: [],
+            tooltip: {
+                custom: function(opts: any) {
+                    return `
+                    <div class="chart__tooltip">
+                        <span>${opts.w.globals.labels[opts.seriesIndex]}</span>
+                        <span>🎖️ ${opts.series[opts.seriesIndex]}</span>
+                    </div>`;
+                }
+            }
         };
-
-        this.olympicsJo = 0;
     }
 
     ngOnInit(): void {
         this.olympics$ = this.olympicService.getOlympics();
-        this.getCountries();
         this.getNumberJo();
+        this.getCountries();
     }
 
-    getCountries(): any {
+    // Get the number of JO's
+    getNumberJo(): void {
         this.olympics$.subscribe((response) => {
-            response?.map((name: any) => {
-                this.olympicCountries.push(name.country);
-                this.getMedals(name.country);
-            });
-
-            return this.olympicCountries;
-        });
-    }
-
-    getNumberJo(): any {
-        this.olympics$.subscribe((response) => {
-            const numberJo = response?.reduce((countries: any, country: any) => {
+            this.numberJo = response?.reduce((countries: any, country: any) => {
                 return (country.participations.length > countries.participations.length ? country : countries);
-            });
-
-            return this.olympicsJo = numberJo?.["participations"].length;
+            })["participations"].length;
         });
     }
 
-    getMedals(countryName: string): any {
+    // Get the countries
+    getCountries(): void {
         this.olympics$.subscribe((response) => {
-            const testa = (response?.find((e: any) => e.country == countryName));
-            testa?.participations.map((abcdef: any) => {
-                this.countriesMedal.push(abcdef.medalsCount)
+            response?.map((data: any) => {
+                this.chartOptions["labels"].push(data.country);
+                this.getMedals(data.country);
             });
         });
-        const abc = this.countriesMedal.reduce((a: any, b: any) => a + b, 0);
-        this.countriesMedals.push(abc);
-        this.countriesMedal = [];
+    }
+
+    // Get the medals per country
+    getMedals(countryName: string): void {
+        this.olympics$.subscribe((response) => {
+            const country = (response?.find((e: any) => e.country == countryName));
+            const countryMedals = country?.participations.map((participation: any) => {
+                return participation.medalsCount;
+            }).reduce((a: any, b: any) => a + b, 0);
+            this.chartOptions['series'].push(countryMedals)
+        });
     }
 }
